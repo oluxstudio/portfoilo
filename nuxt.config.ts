4dd4fac33c7e2bba9a924ac0ce7e5701dcc7792b
oluxstudio <oluxstudio@gmail.com>
2026-08-06 02:43:21 +0100

    fix favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v=2' }],
+    },
+  },
   css: ['@/assets/styles/main.css'],
   modules: ['@pinia/nuxt'],
   postcss: {
